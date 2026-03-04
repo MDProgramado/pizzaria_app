9a0c33a631ddb9b74aa85dbd6ad78f44544d76f9
Maicon Douglas <mdprogamacao@outlook.com>
2026-03-04 11:38:18 -0300

    primeiro commit com a estrutura inicial do banco

---
+++ queries/Query.sql
@@ -0,0 +1,140 @@
+CREATE DATABASE pizzaria_db;
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+
+CREATE  TABLE IF NOT EXISTS  tenants(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    subdomain VARCHAR(100) UNIQUE,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS customers (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    tenant_id uuid NOT NULL REFERENCES tenants(id),
+    name VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ,
+    UNIQUE(tenant_id, phone)
+
+);
+
+
+CREATE TABLE IF NOT EXISTS ingredients (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    tenant_id uuid NOT NULL REFERENCES tenants(id),
+    name VARCHAR(100) NOT NULL,
+    unit_measure VARCHAR(10) NOT NULL,
+    current_balance DECIMAL(15,3) DEFAULT 0,
+    min_threshold DECIMAL(15,3) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ
+);
+
+
+CREATE TABLE IF NOT EXISTS products (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    tenant_id uuid NOT NULL REFERENCES tenants(id),
+    name VARCHAR(225) NOT NULL,
+    base_price DECIMAL(12,2) NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ
+);
+
+
+CREATE TABLE IF NOT EXISTS products_recipes (
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
+    ingredient_id UUID REFERENCES ingredients(id) ON DELETE CASCADE,
+    quantity_required DECIMAL(15,3) NOT NULL,
+    PRIMARY KEY (product_id, ingredient_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS orders (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    tenant_id uuid NOT NULL REFERENCES tenants(id),
+    customer_id UUID REFERENCES customers(id),
+    order_status VARCHAR(20) DEFAULT 'pending' 
+        CHECK (order_status IN ('pending', 'preparing', 'shipped', 'delivered', 'cancelled')),
+    total_price DECIMAL(12,2) NOT NULL,
+    delivery_address_snapshot JSONB NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+
+CREATE TABLE IF NOT EXISTS order_items (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    tenant_id uuid NOT NULL REFERENCES tenants(id),
+    order_id UUID REFERENCES orders(id) ON DELETE CASCADE,
+    product_id UUID REFERENCES products(id),
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
+    unit_price_at_sale DECIMAL(12,2) NOT NULL,
+    subtotal DECIMAL(12,2) GENERATED ALWAYS AS (quantity * unit_price_at_sale) STORED
+);
+
+
+CREATE INDEX idx_orders_customer_id ON orders(customer_id);
+CREATE INDEX idx_orders_status ON orders(order_status) WHERE order_status != 'delivered';
+CREATE INDEX idx_orders_tenant_date ON orders (tenant_id, created_at DESC);
+
+
+CREATE INDEX idx_products_tenant ON products (tenant_id, updated_at);
+CREATE INDEX idx_ingredients_low_stock ON ingredients(current_balance) WHERE current_balance <= min_threshold;
+
+
+
+
+ALTER TABLE customers ENABLE ROW LEVEL SECURITY;
+ALTER TABLE products ENABLE ROW LEVEL SECURITY;
+ALTER TABLE ingredients ENABLE ROW LEVEL SECURITY;
+ALTER TABLE orders ENABLE ROW LEVEL SECURITY;
+
+
+CREATE POLICY tenant_isolation_policy ON customers
+    USING (tenant_id = current_setting('app.current_tenant')::uuid);
+
+CREATE POLICY tenant_isolation_policy ON products
+    USING (tenant_id = current_setting('app.current_tenant')::uuid);
+
+
+CREATE POLICY tenant_isolation_policy ON ingredients
+    USING (tenant_id = current_setting('app.current_tenant')::uuid);
+
+
+CREATE POLICY tenant_isolation_policy ON orders
+    USING (tenant_id = current_setting('app.current_tenant')::uuid);
+
+CREATE POLICY tenant_isolation_policy ON order_items
+    USING (tenant_id = current_setting('app.current_tenant')::uuid);
+
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER set_timestamp_tenants BEFORE UPDATE ON tenants         FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+CREATE TRIGGER set_timestamp_products BEFORE UPDATE ON products       FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+CREATE TRIGGER set_timestamp_customers BEFORE UPDATE ON customers     FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+CREATE TRIGGER set_timestamp_ingredients BEFORE UPDATE ON ingredients FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+CREATE TRIGGER set_timestamp_orders BEFORE UPDATE ON orders           FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+CREATE TRIGGER set_timestamp_order_items BEFORE UPDATE ON order_items FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+
+
+
+
+
+
